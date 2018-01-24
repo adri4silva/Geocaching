627b44f77341b75394744a3d67f028373e39c60e
adri4silva <adri4silva@gmail.com>
2018-01-24 10:56:11 +0100

    Map Kit view implemented

--- Geocaching/Controller/MapViewController.swift
+++ Geocaching/Controller/MapViewController.swift
@@ -7,125 +7,48 @@
 //
 
 import UIKit
-import GoogleMaps
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
 
-    var locationManager = CLLocationManager()
-    var currentLocation: CLLocation?
-    var mapView: GMSMapView!
-    var zoomLevel: Float = 15.0
-    var treasure: Treasure!
-    let vigoMarker = GMSMarker()
+    let locationManager = CLLocationManager()
+    let regionRadius: CLLocationDistance = 1000
+    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+
+
+    /*        treasure.latitude = 37.332350
+     treasure.longitude = -122.031999
+     treasure.name = "Apple Infinite Loop"
+     treasure.info = "Un tesoro en Cupertino. Su historia es increible."*/
+
+
+
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Initialize the location manager.
-        locationManager = CLLocationManager()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
-        locationManager.distanceFilter = 50
-        locationManager.startUpdatingLocation()
-        locationManager.delegate = self
-
-
-
-
-        // Create a map.
-        let camera = GMSCameraPosition.camera(withLatitude: 0,
-                                              longitude: 0,
-                                              zoom: zoomLevel)
-        mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
-        mapView.settings.myLocationButton = true
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.isMyLocationEnabled = true
-        mapView.delegate = self
-
-        treasure.latitude = 37.332350
-        treasure.longitude = -122.031999
-        treasure.name = "Apple Infinite Loop"
-        treasure.info = "Un tesoro en Cupertino. Su historia es increible."
-
-
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: treasure.latitude, longitude: treasure.longitude)
-        marker.title = treasure.name
-        marker.map = mapView
-
-
-        vigoMarker.position = CLLocationCoordinate2D(latitude: 42.223898, longitude: -8.673432)
-        vigoMarker.title = "Casa"
-        vigoMarker.map = mapView
-
-        // Add the map to the view, hide it until we've got a location update.
-        view.addSubview(mapView)
-        mapView.isHidden = true
+        centerMapOnLocation(location: initialLocation)
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "goToTreasureDetails" {
-            let treasureViewController = segue.destination as! TreasureInfoViewController
-            treasureViewController.treasure = treasure
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
     }
 
-
-
-
-
-
-}
-
-extension MapViewController: CLLocationManagerDelegate {
-
-    // Handle incoming location events.
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location: CLLocation = locations.last!
-        print("Location: \(location)")
-
-        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
-                                              longitude: location.coordinate.longitude,
-                                              zoom: zoomLevel)
-
-        if mapView.isHidden {
-            mapView.isHidden = false
-            mapView.camera = camera
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
         } else {
-            mapView.animate(to: camera)
+            locationManager.requestWhenInUseAuthorization()
         }
     }
 
-    // Handle authorization for the location manager.
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        switch status {
-        case .restricted:
-            print("Location access was restricted.")
-        case .denied:
-            print("User denied access to location.")
-            // Display the map using the default location.
-            mapView.isHidden = false
-        case .notDetermined:
-            print("Location status not determined.")
-        case .authorizedAlways: fallthrough
-        case .authorizedWhenInUse:
-            print("Location status is OK.")
-        }
-    }
-
-    // Handle location manager errors.
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        locationManager.stopUpdatingLocation()
-        print("Error: \(error)")
-    }
-}
-
-extension MapViewController: GMSMapViewDelegate {
-
-    // MARK: GMSMapViewDelegate
-    func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
-        self.performSegue(withIdentifier: "goToTreasureDetails", sender: self)
-        return true
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
 }
